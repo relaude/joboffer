6bef0973d46b23c66bf25f5044d78b6d4a5100b0
Ramil Laude <c_relaude@unilab.com.ph>
2026-02-09 13:58:04 +0800

    Setup SBAdmin2 Layout

---
+++ JobOfferSolution/JO.BlazorApp/wwwroot/sbadmin2/js/custom.js
@@ -0,0 +1,31 @@
+﻿function Logout() {
+    $.ajax({
+        url: '/api/auth/logout',
+        method: 'POST',
+        credentials: 'include',
+
+        success: function (response) {
+            window.location.href = '/identity/account/login';
+        },
+        error: function (xhr) {
+            Swal.fire({
+                icon: 'error',
+                title: options.failureTitle || 'Error',
+                text: xhr.responseText || 'An error occurred.'
+            });
+        }
+    });
+}
+
+window.initTooltips = () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+};
+
+
+
+
+
+
