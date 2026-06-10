47bffbef3b269474229f0adfd7992b8f830cf4f5
Ramil Laude <c_relaude@unilab.com.ph>
2026-06-10 17:02:33 +0800

    stop candidate mass upload features

---
+++ JobOfferSolution/JO.DataModel/DTOs/CandidateMassUploadModalDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JO.DataModel.DTOs
+{
+    public class CandidateMassUploadModalDto
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+        public string? ContactNumber { get; set; }
+
+        public decimal? ExpectedSalary { get; set; }
+
+        public int? JobFamilyId { get; set; }
+        public int? JobPositionId { get; set; }
+    }
+}
