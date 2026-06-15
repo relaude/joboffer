101d93abc150282fe483b45a3257924c12f069e7
Rmail Laude <laude.ramil@gmail.com>
2026-06-15 21:00:30 +0800

    processing stage mock up pages

--- JobOfferSolution/JO.Service/Constants/JORoutes.cs
+++ JobOfferSolution/JO.Service/Constants/JORoutes.cs
@@ -11,6 +11,7 @@ namespace JO.Service.Constants
         private const string PrefixJobOffer = "/joboffer";
         private const string PrefixHROD = "/hrod";
         private const string PrefixDH = "/dh";
+        private const string PrefixMock = "/mock";
 
         public static class Public
         {
@@ -45,8 +46,6 @@ namespace JO.Service.Constants
             public const string Return = PrefixTA + "/joboffer/return";
             public const string Email = PrefixTA + "/email";
             public const string MassUpload = PrefixTA + "/mass-upload";
-            public const string MSForms = PrefixTA + "/candidate/ms-forms";
-            public const string MSFormsDashboard = PrefixTA + "/ms-forms";
         }
 
         public static class HROD
@@ -61,6 +60,19 @@ namespace JO.Service.Constants
             public const string Dashboard = PrefixDH + "/dashboard";
             public const string Approvals = PrefixDH + "/approvals";
             public const string Approve = PrefixDH + "/approve";
+        }
+
+        public static class MockUp
+        {
+            public const string FormRequest = PrefixMock + "/form-request";
+            public const string SendRequest = PrefixMock + "/send-request";
+            public const string Legal = PrefixMock + "/legal";
+            public const string Analysis = PrefixMock + "/analysis";
+            public const string Approval = PrefixMock + "/approval";
+            public const string Accept = PrefixMock + "/accept";
+            public const string Negotiate = PrefixMock + "/negotiate";
+            public const string TADashboard = PrefixMock + "/ta-dashboard";
+            public const string ApproverDashboard = PrefixMock + "/approver-dashboard";
         }
     }
 }
