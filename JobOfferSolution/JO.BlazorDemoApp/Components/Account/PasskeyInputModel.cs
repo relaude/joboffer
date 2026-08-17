dabfb7a8596ae652c0970bed12ce6307bd8d5c98
Ramil Laude <c_relaude@unilab.com.ph>
2026-08-17 13:45:03 +0800

    add new project demo app

---
+++ JobOfferSolution/JO.BlazorDemoApp/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace JO.BlazorDemoApp.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ JobOfferSolution/JO.BlazorDemoApp/wwwroot/sbadmin2/js/custom.js
@@ -0,0 +1,41 @@
+﻿window.initTooltips = () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+};
+
+
+window.auth = {
+    login: async function (email) {
+        const res = await fetch('/api/account/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify({
+                email: email
+            })
+        });
+
+        if (!res.ok) return false;
+
+        try {
+            const data = await res.json();
+            return data === true;
+        } catch {
+            return false;
+        }
+    },
+    logout: async function () {
+        await fetch('/api/account/logout', {
+            method: 'POST',
+            credentials: 'include'
+        });
+    }
+};
+
+
+
+
+
+
